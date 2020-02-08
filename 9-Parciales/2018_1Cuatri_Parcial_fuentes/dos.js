da97f272e1a712b1013db4a73f24906dad087322
alumno <alumno@utnfra.com.ar>
2020-02-07 22:03:00 -0300

    Todos los ejercicios + 2 parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,11 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+  document.getElementById('elNombre').value = nombre;
+  document.getElementById("laLocalidad").value = localidad;
+
+  alert(`Usted es ${nombre} y vive en la localidad de ${localidad}` );
+
 }
