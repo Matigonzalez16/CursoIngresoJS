be65397036b4f1943cd6305a4ea1eaf535478344
alumno <alumno@utnfra.com.ar>
2020-02-14 20:03:29 -0300

    Todos los ejercicios de switch

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var nota;
+	nota = Math.floor(Math.random() * 10) +1;
+	console.log(nota);
+	if(nota > 8)
+	{
+		alert(`${nota} : EXCELENTE`);
+	}
+else
+{
+	if(nota >=4){
+		alert(`${nota} : APROBO`);
+	}
+	else{
+		alert(`${nota} : Vamos, la proxima se puede`)
+	}
+}
 }//FIN DE LA FUNCIÓN
+
+// switch(variable){}
+// case "Enero":
+// break;
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numeroAleatorio;
+	var maximoExcluido = 11;
+	var minimoExcluido = 1;
+	//numeroAleatorio = Math.floor(Math.random() * (maximoExcluido - minimoExcluido)) + 1;
+	numeroAleatorio = Math.random();
+	numeroAleatorio *= maximoExcluido - minimoExcluido;
+	numeroAleatorio = Math.floor(numeroAleatorio);
+	numeroAleatorio += minimoExcluido;
+	alert(numeroAleatorio);
+
+	//	 numero = numero + 1;
+	//	 numero += 1;
+	//	 numero ++;
+	// Math.floor = redondea al entero mas cercano hacia abajo  y Math.ceil= redondea al entero mas cercano hacia arriba
+
 
 }//FIN DE LA FUNCIÓN
